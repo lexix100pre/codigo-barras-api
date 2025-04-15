04f50951372d1ef5b4120ae23a69cac55514f2d9
Recepcion BSF <recepcionbsf@farmaciasperuanas.pe>
2025-04-15 10:04:32 -0500

    Primera subida de mi API de código de barras

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, send_file
+import barcode
+from barcode.writer import ImageWriter
+import io
+
+app = Flask(__name__)
+
+@app.route("/barcode")
+def generar_barcode():
+    data = request.args.get("data", "SIN-DATO")
+    if not data:
+        return "❌ No se proporcionó código", 400
+
+    buffer = io.BytesIO()
+    code128 = barcode.get_barcode_class('code128')
+    codigo = code128(data, writer=ImageWriter())
+    codigo.write(buffer)
+    buffer.seek(0)
+
+    return send_file(buffer, mimetype='image/png')
+
+if __name__ == "__main__":
+    app.run(debug=True)
